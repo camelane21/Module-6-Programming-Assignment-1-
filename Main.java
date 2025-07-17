5831930eb70d5e615c91d04a14e5ef5405284df3
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-07-17 13:51:05 -0400

    Update Main.java
    change

--- Main.java
+++ Main.java
@@ -24,3 +24,53 @@ public class Main extends Application {
     private TextField txtState = new TextField();
     private TextField txtPhone = new TextField();
     private TextField txtEmail = new TextField();
+    
+    @Override
+    public void start(Stage primaryStage) {
+        // Top: ID + View button
+        HBox top = new HBox(8,
+            new Label("Record ID:"), txtID,
+            makeButton("View",  e -> doView())
+        );
+        top.setAlignment(Pos.CENTER_LEFT);
+        top.setPadding(new Insets(10));
+
+        // Center: form grid
+        GridPane form = new GridPane();
+        form.setHgap(10);
+        form.setVgap(8);
+        form.setPadding(new Insets(0, 10, 10, 10));
+
+        form.addRow(0, new Label("Last Name:"),   txtLast);
+        form.addRow(1, new Label("First Name:"),  txtFirst);
+        form.addRow(2, new Label("M.I.:"),        txtMI);
+        form.addRow(3, new Label("Address:"),     txtAddr);
+        form.addRow(4, new Label("City:"),        txtCity);
+        form.addRow(5, new Label("State:"),       txtState);
+        form.addRow(6, new Label("Telephone:"),   txtPhone);
+        form.addRow(7, new Label("Email:"),       txtEmail);
+
+        ColumnConstraints leftCol = new ColumnConstraints();
+        leftCol.setMinWidth(Region.USE_PREF_SIZE);
+        form.getColumnConstraints().addAll(leftCol, new ColumnConstraints(300));
+
+        // Bottom: Insert, Update, Clear
+        HBox bottom = new HBox(10,
+            makeButton("Insert", e -> doInsert()),
+            makeButton("Update", e -> doUpdate()),
+            makeButton("Clear",  e -> clearFields())
+        );
+        bottom.setAlignment(Pos.CENTER);
+        bottom.setPadding(new Insets(0, 0, 10, 0));
+
+        // Assemble the scene
+        BorderPane root = new BorderPane();
+        root.setTop(top);
+        root.setCenter(form);
+        root.setBottom(bottom);
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Staff Manager");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
