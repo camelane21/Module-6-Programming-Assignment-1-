fb280d40e76aa322adc2c3a8eaefc44e6e1378e5
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-07-17 13:46:21 -0400

    Update Main.java
    change

--- Main.java
+++ Main.java
@@ -7,3 +7,20 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.sql.*;
+
+public class Main extends Application {
+    // TODO: adjust these for your environment
+    private static final String DB_URL  = "jdbc:mysql://localhost:3306/yourDatabase";
+    private static final String DB_USER = "yourUser";
+    private static final String DB_PASS = "yourPass";
+
+    // UI fields
+    private TextField txtID    = new TextField();
+    private TextField txtLast  = new TextField();
+    private TextField txtFirst = new TextField();
+    private TextField txtMI    = new TextField();
+    private TextField txtAddr  = new TextField();
+    private TextField txtCity  = new TextField();
+    private TextField txtState = new TextField();
+    private TextField txtPhone = new TextField();
+    private TextField txtEmail = new TextField();
